fac40db3613d8f07a51df924b2af22ad815de79c
Bhavana23-05-08 <bhavanaveerisetty2008@gmail.com>
2026-08-29 22:01:12 +0530

    ✅ Java Int to String [java] — approach1

---
+++ solutions/java/easy/java-int-to-string/solution-approach1.java
@@ -0,0 +1,22 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-int-to-string/problem?isFullScreen=true
+// Problem     Java Int to String
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-29, 10:01 p.m.
+// ──────────────────────────────────────────────────
+
+
+   String s =Integer.toString(n) ;
+   //Write your code here
+   if(n>=-100&&n<=100){
+   
+   // System.out.println("Good job");
+   }
+   else{
+    System.out.println("Wrong answer");
+   }
+
